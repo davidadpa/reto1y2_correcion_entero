ed4a2894654d2e0ce5b3a46aae19a7a4475436ff
David <David@LAPTOP-UFCK75MQ>
2022-07-04 09:25:18 -0500

    Carga Inicial

---
+++ src/main/java/com/mycompany/reto1y2/Reto1y2.java
@@ -0,0 +1,13 @@
+package com.mycompany.reto1y2;
+
+/**
+ *
+ * @author valeria Milanés
+ */
+public class Reto1y2 {
+
+    public static void main(String[] args) {
+        Clscalculadora calculadora = new Clscalculadora();
+        calculadora.iniciarPrograma();
+    }
+}
